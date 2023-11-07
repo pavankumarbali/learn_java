eb46f3df60bf53bbe556ef2d8536817349c7bce4
pavankumarbali <pavankumarbali2002@gmail.com>
2023-11-07 22:32:57 +0530

    first commit

---
+++ first_program.java
@@ -0,0 +1,14 @@
+/**
+ * first_program
+ */
+import java.util.*;
+public class first_program {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 Numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("VAL A :" +a);
+        System.out.println("VAL B " +b);
+    }
+}
