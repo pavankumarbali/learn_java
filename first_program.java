5a75e96b4a6824700f7d341500a64c3a5bc63efe
pavankumarbali <pavankumarbali2002@gmail.com>
2023-11-21 20:03:21 +0530

    new files

--- first_program.java
+++ first_program.java
@@ -3,12 +3,16 @@
  */
 import java.util.*;
 public class first_program {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter 2 Numbers");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        System.out.println("VAL A :" +a);
-        System.out.println("VAL B " +b);
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter 2 Numbers");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int c=a*b;
+System.out.println("VAL A :" +a);
+System.out.println("VAL B " +b);
+
     }
 }
